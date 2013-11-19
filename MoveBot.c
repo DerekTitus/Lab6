67da7be87caaa17c37d33604110c0ebb2712a62a
Derek Titus <c15derek.titus@usafa.edu>
2013-11-19 15:19:37 -0700

    I added the timer A code and wrote down the general idea for subroutines I would use such as moving forward, backwards and turning.

---
+++ MoveBot.c
@@ -0,0 +1,26 @@
+/*
+ * MoveBot.c
+ *
+ *  Created on: Nov 18, 2013
+ *      Author: C15Derek.Titus
+ */
+
+	MoveForward(seconds){
+		// set motor one pwm and ground
+		// set motor two pwm and ground
+	}
+
+	MoveBack(seconds){
+		// opposite sides for pwm and ground for both motors
+	}
+
+	TurnRight(degrees){
+		// pwm on opposite sides for motors. top left and bottom right
+	}
+
+	TurnLeft(degrees){
+		// pwm on opposite sides for motors. top right and bottom left
+	}
+
+
+
---
+++ MoveBot.h
@@ -0,0 +1,14 @@
+/*
+ * MoveBot.h
+ *
+ *  Created on: Nov 18, 2013
+ *      Author: C15Derek.Titus
+ */
+
+#ifndef MOVEBOT_H_
+#define MOVEBOT_H_
+
+
+
+
+#endif /* MOVEBOT_H_ */
