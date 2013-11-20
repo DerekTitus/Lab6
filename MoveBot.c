bef0e41a67371dee31dcd719fc4b695c8067f03e
Derek Titus <c15derek.titus@usafa.edu>
2013-11-20 13:21:57 -0700

    movement code

--- MoveBot.c
+++ MoveBot.c
@@ -4,23 +4,99 @@
  *  Created on: Nov 18, 2013
  *      Author: C15Derek.Titus
  */
+int loop(void)
+{
 
-	MoveForward(seconds){
-		// set motor one pwm and ground
-		// set motor two pwm and ground
+    P1DIR |= BIT1;				//TA0.0 on P1.1
+    P1SEL |= BIT1;              //TA0.0 on P1.1
+
+    P1DIR |= BIT2;                // TA0CCR1 on P1.2
+    P1SEL |= BIT2;                // TA0CCR1 on P1.2
+
+    TACTL &= ~(MC1|MC0);            // stop timer A0
+
+    TACTL |= TACLR;                // clear timer A0
+
+    TACTL |= TASSEL1;           // configure for SMCLK
+
+    TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+    TACCR1 = 50;                // set duty cycle to 25/100 (25%)
+
+    TACCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+    TACCTL0 |= OUTMOD_5;
+
+    TACTL |= MC0; //count up
+
+
+    P2DIR |= BIT0;				//TA0.0 on P1.1
+    P2SEL |= BIT0;              //TA0.0 on P1.1
+
+    P2DIR |= BIT1;                // TA0CCR1 on P1.2
+    P2SEL |= BIT1;                // TA0CCR1 on P1.2
+
+    TA1CTL &= ~(MC1|MC0);            // stop timer A0
+
+    TA1CTL |= TACLR;                // clear timer A0
+
+    TA1CTL |= TASSEL1;           // configure for SMCLK
+
+    TA1CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+    TA1CCR1 = 50;                // set duty cycle to 25/100 (25%)
+
+    TA1CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+    TA1CCTL0 |= OUTMOD_5;
+
+    TA1CTL |= MC0; //count up
+    TACTL |= MC0;                // count up
+
+
+	RightMotorForward(){
+		TA1CCTL1 &= ~OUTMOD0;
+		TA1CCTL1 |= OUTMOD_4;        // set TACCTL1 to Reset mode
+		TA1CCTL0 &= ~OUTMOD0;
+		TA1CCTL0 |= OUTMOD_5;
 	}
 
-	MoveBack(seconds){
-		// opposite sides for pwm and ground for both motors
+	RightMotorBackward(){
+		TA1CCTL1 &= ~OUTMOD0;
+		TA1CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+	    TA1CCTL0 &= ~OUTMOD0;
+		TA1CCTL0 |= OUTMOD_4;
 	}
 
-	TurnRight(degrees){
-		// pwm on opposite sides for motors. top left and bottom right
+	LeftMotorForward(){
+		TACCTL1 &= ~OUTMOD0;
+		TACCTL1 |= OUTMOD_4;        // set TACCTL1 to Reset / Set mode
+		TACCTL0 &= ~OUTMOD0;
+		TACCTL0 |= OUTMOD_5;
 	}
 
-	TurnLeft(degrees){
-		// pwm on opposite sides for motors. top right and bottom left
+	LeftMotorBackward(){
+		TACCTL1 &= ~OUTMOD0;
+		TACCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+		TACCTL0 &= ~OUTMOD0;
+		TACCTL0 |= OUTMOD_4;
+	}
+	MoveForward(){
+		RightMotorForward();
+		LeftMotorForward();
+	}
+
+	MoveBack(){
+		RightMotorBackward();
+		LeftMotorBackward();
+	}
+
+	TurnRight(){
+		RightMotorBackward();
+		LeftMotorForward();
+	}
+
+	TurnLeft(){
+		RightMotorForward();
+		LeftMotorBackward();
 	}
 
 
+}
 
--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
+    P1DIR |= BIT1;				//TA0.0 on P1.1
+    P1SEL |= BIT1;              //TA0.0 on P1.1
+
     P1DIR |= BIT2;                // TA0CCR1 on P1.2
     P1SEL |= BIT2;                // TA0CCR1 on P1.2
 
-    TACTL &= ~MC1|MC0;            // stop timer A0
+    TACTL &= ~(MC1|MC0);            // stop timer A0
 
     TACTL |= TACLR;                // clear timer A0
 
@@ -18,7 +21,33 @@ int main(void) {
     TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
     TACCR1 = 50;                // set duty cycle to 25/100 (25%)
 
-    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+    TACCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+    TACCTL0 |= OUTMOD_5;
+
+    TACTL |= MC0; //count up
+
+
+    P2DIR |= BIT0;				//TA0.0 on P1.1
+    P2SEL |= BIT0;              //TA0.0 on P1.1
+
+    P2DIR |= BIT1;                // TA0CCR1 on P1.2
+    P2SEL |= BIT1;                // TA0CCR1 on P1.2
+
+    TA1CTL &= ~(MC1|MC0);            // stop timer A0
+
+    TA1CTL |= TACLR;                // clear timer A0
+
+    TA1CTL |= TASSEL1;           // configure for SMCLK
+
+    TA1CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+    TA1CCR1 = 50;                // set duty cycle to 25/100 (25%)
+
+    TA1CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+    TA1CCTL0 |= OUTMOD_5;
+
+    TA1CTL |= MC0; //count up
+
+
 
     TACTL |= MC0;                // count up
 
