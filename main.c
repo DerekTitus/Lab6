13230b0806a96cd85cc5f95955925aba5c24dffc
Derek Titus <c15derek.titus@usafa.edu>
2013-11-20 08:08:28 -0700

    I set up all of my timers. They need to be transfered to my .c but at least I have the code at this point.

--- main.c
+++ main.c
@@ -5,6 +5,24 @@
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+    P1DIR |= BIT2;                // TA0CCR1 on P1.2
+    P1SEL |= BIT2;                // TA0CCR1 on P1.2
+
+    TACTL &= ~MC1|MC0;            // stop timer A0
+
+    TACTL |= TACLR;                // clear timer A0
+
+    TACTL |= TASSEL1;           // configure for SMCLK
+
+    TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+    TACCR1 = 50;                // set duty cycle to 25/100 (25%)
+
+    TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+
+    TACTL |= MC0;                // count up
+
 	
+    moverobot();
 	return 0;
 }
